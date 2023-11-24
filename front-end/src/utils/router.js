2e937fbfc46bf944c25a396093c8ac7ccf447750
Manhth99 <manhth99@gmail.com>
2023-11-24 20:46:37 +0700

    fe-demo

---
+++ front-end/src/utils/router.js
@@ -0,0 +1,12 @@
+export const ROUTERS = {
+    USER : {
+        HOME : "",
+        PROFILE: "profile",
+        SHOP: "shop",
+        POSTS: "post",
+        CONTACT: "contact",
+        BRAND: "brand",
+        
+        
+    }
+}
